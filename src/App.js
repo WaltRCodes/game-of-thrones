6ed96e4d2826e92309a6ab04cfd46f26c67c5a8a
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-01 12:44:55 -0400

    started pulling data

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ export default class App extends Component {
       "What is the second seat of House Baratheon?",
       "What is Robert Baratheon's second alias?",
       "What's the name of the founder of House Stark?",
-      "What are the titles of Catelyn Stark's three POV books? "]
+      "What are the titles of Catelyn Stark's three POV books? "],
+      answers:[]
     }
   }
 
   componentDidMount() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    //url origin: https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
     let MargaeryTyrell = proxyurl+ "https://anapioficeandfire.com/api/characters/16";
     let HouseTargaryen = "https://www.anapioficeandfire.com/api/houses/378";
     let HouseLannister = "https://www.anapioficeandfire.com/api/houses/229";
@@ -34,6 +36,28 @@ export default class App extends Component {
     axios.all(calls).then(axios.spread((...responses) => {
       const firstResponse = responses[0];
       console.log(firstResponse);
+      console.log(firstResponse.data.born);
+      const answerOne = firstResponse.data.born;
+      const secondResponse = responses[1];
+      console.log(secondResponse.data.region);
+      const thirdResponse = responses[2];
+      console.log(thirdResponse.data.coatOfArms);
+      const forthResponse = responses[3];
+      console.log(forthResponse.data.seats[1]);
+      const fifthResponse = responses[4];
+      console.log(fifthResponse.data.aliases[1]);
+      const sixResponse = responses[5];
+      //console.log(sixResponse.data);
+      axios.get(sixResponse.data.founder)
+      .then(res => {
+        const name = res.data.name;
+        console.log("founder",name)
+      })
+      .catch(error => {
+        console.log('there is an eror', error)
+      })
+      const seventhResponse = responses[6];
+      //console.log(seventhResponse.data);
       
     })).catch(errors => {
       console.log(errors);
