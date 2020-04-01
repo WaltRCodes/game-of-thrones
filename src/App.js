5c07e161eeca5ed8c26adc337b70d4f3074ecb7d
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-01 11:57:49 -0400

    changed app.js into a class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1> Questions about Game of Thrones</h1>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
