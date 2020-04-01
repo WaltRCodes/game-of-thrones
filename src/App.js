cc666a04cb38e4d9af06abfbadda2270f762da5c
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-01 12:06:26 -0400

    added urls

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ export default class App extends Component {
 
     this.state = {
       isLoaded: false,
-      data: []
+      data: [],
+      questions: ["",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""]
     }
   }
 
   componentDidMount() {
-
+    let MargaeryTyrell = "http://anapioficeandfire.com/api/characters/16";
+    let HouseTargaryen = "http://www.anapioficeandfire.com/api/houses/378";
+    let HouseLannister = "http://www.anapioficeandfire.com/api/houses/229";
+    let HouseBaratheon ="http://www.anapioficeandfire.com/api/houses/17";
+    let RobertBaratheon = "http://www.anapioficeandfire.com/api/characters/901";
+    let HouseStark = "http://www.anapioficeandfire.com/api/houses/362";
+    let CatelynStark = "http://www.anapioficeandfire.com/api/characters/232";
 
   }
 
